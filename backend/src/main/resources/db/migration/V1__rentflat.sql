b0c8df8ea0d52edbbec195100b9517054c641fc8
Mariusz Figiel <mariusz.figiel@mailfence.com>
2019-11-04 09:03:31 +0100

    V1 sql script for flyway

--- backend/src/main/resources/db/migration/V1__rentflat.sql
+++ backend/src/main/resources/db/migration/V1__rentflat.sql
@@ -13,19 +13,35 @@ CREATE TABLE room (
     FOREIGN KEY (flat_id) REFERENCES flat(id)
 );
 
-CREATE VIEW room_rent_cost AS
-    SELECT
-        r.id as room_id,
-        f.id as flat_id,
-        (
-            f.cost/f.surface
-        )*(
-            r.surface + (
-                f.surface - (SELECT sum(surface) FROM room WHERE flat_id = f.id)
-                )/(
-                    SELECT count(*) FROM room WHERE flat_id = f.id
-                    )
-        ) as room_cost
-    FROM  flat f JOIN room r on f.id = r.flat_id
-    GROUP BY f.id, r.id
-    ORDER BY f.id;;
+CREATE MATERIALIZED VIEW room_rent_cost AS
+    SELECT flat_id,
+           room_id,
+           cost_per_surface*(room_surface + common_part_surface/room_amount) as room_cost
+    FROM (SELECT
+              f.id as flat_id,
+              r.id as room_id,
+              (f.surface - (SELECT sum(surface) FROM room WHERE flat_id = f.id)) as common_part_surface,
+              (SELECT count(*) FROM room WHERE flat_id = f.id) as room_amount,
+              r.surface as room_surface,
+              (f.cost/f.surface) as cost_per_surface
+          FROM  flat f JOIN room r on f.id = r.flat_id) AS help_table;
+
+CREATE OR REPLACE FUNCTION refresh_room_rent_cost_view()
+    RETURNS trigger AS
+$$
+BEGIN
+    REFRESH MATERIALIZED VIEW room_rent_cost;
+    RETURN null;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER refresh_room_rent_cost_room
+    AFTER INSERT OR UPDATE OR DELETE
+    ON room
+EXECUTE FUNCTION refresh_room_rent_cost_view();
+
+CREATE TRIGGER refresh_room_rent_cost_flat
+    AFTER UPDATE OR DELETE
+    ON flat
+EXECUTE FUNCTION refresh_room_rent_cost_view();
+
