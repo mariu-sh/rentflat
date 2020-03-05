68a36c8e9661ad61860a1c8f9684fc26e6766947
Kacper Borkowski <kacper.borkowski@unity.pl>
2020-03-05 22:07:15 +0100

    Add TS for project

--- frontend/src/containers/Navigation/Sidebar.tsx
+++ frontend/src/containers/Navigation/Sidebar.tsx
@@ -2,13 +2,23 @@ import React from "react";
 
 import styled from "styled-components";
 
+import "./Sidebar.scss";
+
 const Wrapper = styled.nav`
   background: #f3f3f3;
   width: 13%;
+
+  @media (max-width: 800px) {
+    background: black;
+  }
 `;
 
 function Sidebar() {
-  return <Wrapper>nav</Wrapper>;
+  return (
+    <Wrapper>
+      <p className="text-color font-big">nav</p>
+    </Wrapper>
+  );
 }
 
 export default Sidebar;
