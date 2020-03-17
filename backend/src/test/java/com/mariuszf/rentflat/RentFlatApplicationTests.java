d231b4ddb97fe9eb5f47fbb3c10369ebbece8a83
mariu-sh <mariusz.figiel@mailfence.com>
2020-03-17 01:35:22 +0100

    Cleaning up after changes

---
+++ backend/src/test/java/com/mariuszf/rentflat/RentFlatApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mariuszf.rentflat;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RentFlatApplicationTests {
+
+	@Test
+	public void contextLoads(){}
+
+}
